e0412b1ae892ff6d404510fc3b89ecd04f64060a
Dimitar Todorov <d.todorov.vn@gmail.com>
2023-01-22 11:00:37 +0200

    Add initial project

---
+++ src/main/java/com/dst/softuni/softunispringbootfundamentals/SoftuniSpringBootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.dst.softuni.softunispringbootfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftuniSpringBootFundamentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftuniSpringBootFundamentalsApplication.class, args);
+    }
+
+}
